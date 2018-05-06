2bbae6c6ad07adeeb4e7cdea2e65ce67db247cf2
HellooYing <15642330153@163.com>
2018-05-06 14:04:45 +0800

    1

--- server/app.js
+++ server/app.js
@@ -163,3 +163,9 @@ function selectparkingtime(idORparkingORtimeORprice, content, callback) {
 
 var resf = selectparkingtime("id", 2, callbacktest)
 conn.end();
+var request = require('request');
+request('https://api.weixin.qq.com/sns/jscode2session?appid=wx5fbbb4d25168eb48&secret=106619eb311c77a93dbfd697ff06f709&js_code=JSCODE&grant_type=authorization_code', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+});
