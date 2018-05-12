9c68ee1fd655d65955b1521b58142e78bf28f83d
HellooYing <15642330153@163.com>
2018-05-12 21:55:47 +0800

    数据库信息可以显示到页面

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,3 +1,16 @@
+const fc=require('../../server/function')
+const fs = require('fs');
+var zhi
+function c(a){
+    zhi=a
+    console.log(zhi)
+  }
 exports.login = async (ctx, next) => {
-    console.log(ctx.request.body)
+    code=ctx.request.body//具体再改
+    console.log(code)
+    var res = await fc.selectowner("id",1,c);
+		if(res){
+            ctx.response.type = 'html';
+  ctx.response.body = "<h1>"+zhi[0].id+zhi[0].name+zhi[0].idcard+"<h1>";
+		}
 }
