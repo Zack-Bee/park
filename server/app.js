c27a1a85251337481d5a21b51d7a6b7256e86fa0
HellooYing <15642330153@163.com>
2018-05-12 13:57:53 +0800

    ,

--- server/app.js
+++ server/app.js
@@ -4,11 +4,37 @@ const parkingtime=require('../server/models/parkingtime');
 let mysql = require('mysql');
 const fc=require('./owner.js')
 const xcxlogin=require('./login')
+const Koa = require('koa');
+const fs = require('fs');
+
+const app = new Koa();
+
+const main = ctx => {
+  ctx.response.type = 'html';
+  ctx.response.body = fs.createReadStream('./server/test.html');
+};
+app.use(main)
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
+var rows
 function c(option) {
-  rows = option
-  i=0
+  //i=0
   //fc.changeparking(1, 2,rows[i].kind,rows[i].name, rows[i].location,rows[i].lola, rows[i].number, rows[i].kind3_number, rows[i].lease)
-  console.log(rows)
+  rows=option
+  console.log(option)
+  app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>'+rows[0].id+rows[0].name+'</h1>';
+  });
 }
-fc.selectparking("id",1,c)
-xcxlogin.xcxlogin("003BOwXN0ZOh2b2UE20O0BHtXN0BOwXX",c)
+//fc.selectowner("id",1,c)
+
+
+//小程序根据code换openid操作：
+//xcxlogin.xcxlogin("003oxtka1QSV4R1YDgka1fCyka1oxtk6",c)
+// function c(option) {
+//   rows = JSON.parse(option)
+//   console.log(rows,rows.openid)
+// }
--- server/login.js
+++ server/login.js
@@ -2,8 +2,16 @@ var request = require('request');
 exports.xcxlogin=function(code,callback){
 request('https://api.weixin.qq.com/sns/jscode2session?appid=wx5fbbb4d25168eb48&secret=106619eb311c77a93dbfd697ff06f709&js_code='+code+'&grant_type=authorization_code', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        console.log(body);
         callback(body)
     }
 });
 }
+/*在别的js文件中
+const xcxlogin=require('./login')
+xcxlogin.xcxlogin(code,callback)
+code是临时登陆凭证，callback是回调函数，比如
+function c(option) {
+  var xxx = JSON.parse(option)
+}
+xxx.openid即为openid
+*/
