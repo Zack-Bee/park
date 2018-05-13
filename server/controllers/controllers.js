cc7389e8c37deefb5e683c2a77e41fabfd5399fd
HellooYing <15642330153@163.com>
2018-05-13 14:41:02 +0800

    login try

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,16 +1,14 @@
 const fc=require('../../server/function')
 const fs = require('fs');
 var zhi
-function c(a){
-    zhi=a
+function c(option){
+    zhi=JSON.parse(option)
     console.log(zhi)
   }
 exports.login = async (ctx, next) => {
-    code=ctx.request.body//具体再改
-    console.log(code)
-    var res = await fc.selectowner("id",1,c);
+    code=ctx.request.body.code
+    var res = await fc.xcxlogin(code,c);
 		if(res){
-            ctx.response.type = 'html';
-  ctx.response.body = "<h1>"+zhi[0].id+zhi[0].name+zhi[0].idcard+"<h1>";
+  ctx.response.body = zhi.openid;
 		}
 }
