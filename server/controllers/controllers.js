534fb0d8e6b3cbb4c2e6d664c19699e8611fbbb7
HellooYing <15642330153@163.com>
2018-05-13 14:49:03 +0800

    try2

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -9,6 +9,6 @@ exports.login = async (ctx, next) => {
     code=ctx.request.body.code
     var res = await fc.xcxlogin(code,c);
 		if(res){
-  ctx.response.body = zhi.openid;
+      ctx.body ={"openid": zhi.openid}
 		}
 }
