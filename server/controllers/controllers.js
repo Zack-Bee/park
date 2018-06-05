c9865f5dfba798dfa706c0d288961381a6ae6740
hellooying <15642330153@163.com>
2018-06-05 14:14:24 +0800

    .

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -36,7 +36,6 @@ exports.parks = async (ctx, next) => {
   })
   let i=0
   while (i < option.length) {
-    fc.addparkingtime(option[i].id,"1.0.00-7.23.59",5,0,0)
     if (option[i].isOpen == 1) {
       let la1 = option[i].lola.split(",")[0]
       let lo1 = option[i].lola.split(",")[1]
@@ -398,6 +397,7 @@ exports.upload = async (ctx, next) => {
 }
 
 exports.gethistory = async (ctx, next) => {
+  ctx.response.status = 200;
   if (ctx.request.body.type == "get") {
     await fc.selecthistory("openId", "'" + ctx.request.body.openId + "'", function (option) {
       if (option.length <= 10) {
