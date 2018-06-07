bd42d21a26dad40a4b281ed98cae681ff1e4a9e4
hellooying <15642330153@163.com>
2018-06-07 21:09:58 +0800

    .

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -37,9 +37,9 @@ exports.parks = async (ctx, next) => {
       option = opt
     }
   })
-  console.log("外面",option)
   let i = 0
   while (i < option.length) {
+    console.log(i)
     if (option[i].isOpen == 1) {
       let la1 = option[i].lola.split(",")[0]
       let lo1 = option[i].lola.split(",")[1]
